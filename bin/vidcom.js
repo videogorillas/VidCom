f4810051c3199cb0273ecc55c031ac88f21b282c
Genna Linevich <g@linevich.com>
2019-10-14 17:56:50 +0300

    Relative path support. Fixes #1

--- bin/vidcom.js
+++ bin/vidcom.js
@@ -4,7 +4,7 @@ const open = require('open');
 const express = require('express')
 const path = require('path');
 
-if (argv._.length !== 2) {
+if (argv._.length < 2) {
     console.error(`Expecting 2 file paths. Found: ${argv._.length}`);
     process.exit(1);
 }
@@ -13,15 +13,15 @@ const app = express();
 
 const port = argv.port || 8042;
 
-const path0 = path.parse(path.resolve(__dirname, argv._[0]));
-const path1 = path.parse(path.resolve(__dirname, argv._[1]));
+const path0 = path.parse(path.resolve(process.cwd(), argv._[0]));
+const path1 = path.parse(path.resolve(process.cwd(), argv._[1]));
 
 app.get('/', (req, res) => res.sendFile("./dist/index.html", { root: __dirname }));
 app.get('/video1.mp4', (req, res) => res.sendFile(path0.base, { root: path0.dir }));
 app.get('/video2.mp4', (req, res) => res.sendFile(path1.base, { root: path1.dir }));
 
 if (argv._.length > 2) {
-    const srtPath = path.parse(path.resolve(__dirname, argv._[2]));
+    const srtPath = path.parse(path.resolve(process.cwd(), argv._[2]));
     app.get('/subtitles.srt', (req, res) => res.sendFile(srtPath.base, {root: srtPath.dir}));
 }
 
